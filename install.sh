a2dc8d10e1c0166be0f5e7e992772ffd2a477888
Filip Sodić <filip.sodic@fer.hr>
2019-06-16 11:03:59 +0200

    more general home in install.sh

--- install.sh
+++ install.sh
@@ -29,8 +29,8 @@ fi
 
 # link config files
 for FILE_NAME in $(cat files); do
-    TARGET=~/dotfiles/$FILE_NAME
-    LINK=~/$FILE_NAME
+    TARGET="$HOME/dotfiles/$FILE_NAME"
+    LINK="$HOME/$FILE_NAME"
     echo "Creating a symbolic link: $LINK -> $TARGET".
     ln -sf "$TARGET" "$LINK"
 done
