13f913b8360337401c6936e53c201c57a8bbea80
Filip Sodić <filip.sodic@fer.hr>
2022-11-01 13:31:34 +0100

    Differentiate local and remote theme

---
+++ zsh/theme.remote.zsh
@@ -0,0 +1,23 @@
+# to be able to use color names
+autoload -U colors && colors
+
+setopt PROMPT_SUBST
+
+local ret_status="%(?:%{$fg_bold[cyan]%}❯ :%{$fg_bold[red]%}❯ )"
+local git_info='$(git_prompt_info)'
+local dir='%{$fg[blue]%}%c%{$reset_color%}'
+local reset_colors='%{$reset_color%}'
+
+if [[ $UID -eq 0 ]]; then
+    local user_host='%{$terminfo[bold]$fg[red]%}%n@%m'
+else
+    local user_host='%{$terminfo[bold]$fg[cyan]%}%n@%m'
+fi
+
+PROMPT="${user_host} ➜ ${dir} ${git_info}
+${ret_status}${reset_colors}"
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}(%{$fg[yellow]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}✗"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%}) %{$fg[cyan]%}✓"
