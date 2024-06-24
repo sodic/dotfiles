db47d61f646c70c47e8f36466fadcbbb665e2245
Filip Sodić <filip.sodic@fer.hr>
2024-06-24 19:38:07 +0200

    Add note for using gw in vim

--- vim/tj/init.lua
+++ vim/tj/init.lua
@@ -634,6 +634,8 @@ require('lazy').setup({
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for tsserver)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+            -- NOTE: (Filip) After this is called, gq no longer works, I am
+            -- using gw instead
             require('lspconfig')[server_name].setup(server)
           end,
         },
